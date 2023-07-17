e721af2d661b7784f600c21788bdc3b8e1b8ea32
Hakim Teasdell <4keem@Hakims-Mac-mini.local>
2023-07-17 15:31:53 -0400

    Initated React, Tailwind, And Built NavBar

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+
+  return(
+    <div>
+
+      <NavTabs />
+      
+    </div>
+  );
+
+}
